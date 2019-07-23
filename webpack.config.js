44e8080df8bf66a5edf4fc12717d35341ead6ec1
Matthew Alonso <granjef3@gmail.com>
2019-07-23 12:24:29 -0500

    cleanup, call build script better

--- webpack.config.js
+++ webpack.config.js
@@ -11,31 +11,36 @@
  * limitations under the License.
  */
 
-const WebpackShellPlugin = require("webpack-shell-plugin-next");
 const CopyPlugin = require('copy-webpack-plugin');
-const path = require("path");
-
+const path = require('path');
+const spawn = require('child_process').spawnSync;
 
 module.exports = {
-  mode: "development",
-  context: path.resolve(__dirname, "."),
-  devtool: "nosources-source-map",
-  entry: "./index.js",
+  mode: 'development',
+  context: path.resolve(__dirname, '.'),
+  devtool: 'nosources-source-map',
+  entry: './index.js',
   plugins: [
-    new WebpackShellPlugin({
-      onBuildStart: {
-        scripts: ["npm run build"],
-        blocking: true,
-        parallel: false,
+    {
+      apply: (compiler) => {
+        compiler.hooks.compilation.tap('emscripten-build', (compilation) => {
+          let result = spawn('node', ['build.js'], { stdio: 'inherit' });
+          
+          if (result.status != 0) {
+            compilation.errors.push('emscripten build failed');
+          } else {
+            console.log("emscripten build complete")
+          }
+        });
       }
-    }),
+    },
     new CopyPlugin([
       //we need to manually copy this instead of requiring from
       //our script source code, since wasm files are bound to global scope
       //in workers, rather than being fetched like the browser.
       //wranglerjs also needs to see a wasm file in order for it to be sent to the api
       //correctly.
-      { from: './build/module.wasm', to: './worker/module.wasm'},,
+      { from: './build/module.wasm', to: './worker/module.wasm'},
     ])
   ],
   module: {
@@ -45,7 +50,7 @@ module.exports = {
       // as the file name).
       {
         test: /emscripten\.js$/,
-        loader: "exports-loader"
+        loader: 'exports-loader'
       },
     ]
   },
